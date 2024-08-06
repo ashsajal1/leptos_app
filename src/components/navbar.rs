92801a4dac4aabf8243240c87dd86f4df18c11f3
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-08-06 08:16:00 +0600

    🌈 style: navbar

--- src/components/navbar.rs
+++ src/components/navbar.rs
@@ -3,11 +3,12 @@ use leptos::*;
 #[component]
 pub fn Navbar() -> impl IntoView {
     view! {
-        <nav class="p-2 bg-gray-800">
-            <ul>
-                <li><a href="/">Home</a></li>
-                <li><a href="/about">About</a></li>
-                <li><a href="/contact">Contact</a></li>
+        <nav class="p-2 bg-gray-800 navbar flex justify-between items-center">
+            <h3>"Leptos App"</h3>
+            <ul calss="flex items-center gap-2">
+                <li class="btn btn-primary mr-2"><a href="/">Home</a></li>
+                <li class="btn btn-primary mr-2"><a href="/about">About</a></li>
+                <li class="btn btn-primary mr-2"><a href="/contact">Contact</a></li>
             </ul>
         </nav>
     }
